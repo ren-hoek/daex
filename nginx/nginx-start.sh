f2fb098d6038a5e1211df1a06edd788b9f4c946f
ren-hoek <ren-hoek@users.noreply.github.com>
2018-06-25 16:57:40 +0100

    added password authentication to nginx

--- nginx/nginx-start.sh
+++ nginx/nginx-start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-docker run -d -p 80:80 -p 443:443\
-	-v /home/gavin/Projects/prod/nginx/conf:/etc/nginx \
-	-v /home/gavin/Projects/prod/nginx/html:/usr/share/nginx/html \
+docker run -d -p 80:80 -p 443:443 \
+	-v /home/gavin/Projects/daex/nginx/conf:/etc/nginx \
+	-v /home/gavin/Projects/daex/nginx/html:/usr/share/nginx/html \
+	-v /etc:/opt/etc:ro \
 	--name nginx \
 	--hostname nginx \
-	--network jupyterhub nginx
+	--network jupyterhub prod/nginx
 
