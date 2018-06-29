5567171af1966258ffe403275d30e2d542fde8ed
ren-hoek <ren-hoek@users.noreply.github.com>
2018-06-29 09:49:51 +0100

    fixed typo in shiny log mounting

--- shiny/run-shiny.sh
+++ shiny/run-shiny.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --rm -d -p 3838:3838 \
     -v /home/gavin/Projects/daex/shiny/apps/:/srv/shiny-server/ \
-    -v /home/gavin/Projects/deax/shiny/logs/:/var/log/shiny-server/ \
+    -v /home/gavin/Projects/daex/shiny/logs/:/var/log/shiny-server/ \
     --network jupyterhub \
     --name shiny \
     rocker/shiny
